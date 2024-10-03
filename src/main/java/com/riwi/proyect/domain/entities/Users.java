4f041ed3c590a63ae2489e1dba32cd5ebd2e63ab
BrayanConde23 <soycondebrayan@gmail.com>
2024-10-03 17:27:48 -0500

    feat: Add user entity

---
+++ src/main/java/com/riwi/proyect/domain/entities/Users.java
@@ -0,0 +1,34 @@
+package com.riwi.proyect.domain.entities;
+
+import com.riwi.proyect.domain.enums.RoleEnum;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.management.relation.Role;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private RoleEnum role;
+}
