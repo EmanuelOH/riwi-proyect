e13e8ae1f44a965fdc92ac3622084dc712cd0c19
EmanuelOH <osorioemanuel0520@gmail.com>
2024-10-04 19:41:04 -0500

    feat:creation of the auditable and implementation of swagger

---
+++ src/main/java/com/riwi/proyect/infrastructure/config/OpenApiConfig.java
@@ -0,0 +1,38 @@
+package com.riwi.proyect.infrastructure.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(
+        title = "Riwi project api",
+        version = "1.0",
+        description = "API for project and creation task"
+        //contact = @Contact(name = "Soporte", email = "example@example.com")
+),
+        servers = {
+                //@Server(url = "https://api.herotraining.com/v1", description = "Server of production"),
+                @Server(url = "http://localhost:8080", description = "Server local")
+        }
+)
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenApi() {
+        return new OpenAPI() // Crea una nueva instancia de OpenAPI
+                .components( // Define los componentes de la especificación OpenAPI
+                        new Components().addSecuritySchemes("bearerAuth", // Agrega un esquema de seguridad llamado "bearerAuth"
+                                new SecurityScheme() // Crea un nuevo esquema de seguridad
+                                        .type(SecurityScheme.Type.HTTP) // Define el tipo de esquema de seguridad como HTTP
+                                        .scheme("bearer") // Especifica que el esquema de autenticación es de tipo "bearer"
+                                        .bearerFormat("JWT") // Indica que el formato del token es JWT (JSON Web Token)
+                                        .name("Authorization"))); // Establece el nombre del encabezado para pasar el token de autorización
+    }
+
+}
